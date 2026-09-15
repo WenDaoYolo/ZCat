60ea2df3aec991f6c173113340bd9f90479f55d2
WenDaoYolo <3547474131@qq.com>
2026-09-15 21:19:55 +0800

    add port config file

--- ZCatServer/source/main.cpp
+++ ZCatServer/source/main.cpp
@@ -1,8 +1,26 @@
 #include "../include/TcpServer.h"
 
+
+int LoadPort()
+{   
+    std::fstream f1;
+    char port_str[128]={0};
+    f1.open("./CONFIG.txt",std::ios::in);
+    
+    if(f1.is_open())
+    {
+        f1.getline(port_str,128,'\n');
+        memset(port_str,0,sizeof(port_str));
+
+        f1.getline(port_str,128,'\n');
+        f1.close();
+    }
+    return std::stoi(port_str);
+}
+
 int main()
 {
-    TcpServer tcps1(1026,6);
+    TcpServer tcps1(LoadPort(),6);
 
     std::thread AdminPanel([&](void){tcps1.AdminPanel();});
     tcps1.StartUp();
