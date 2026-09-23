16729d921ac39164326f03589232fab8a1d729f0
WenDaoYolo <3547474131@qq.com>
2026-09-23 12:29:41 +0800

    final

---
+++ ZCatClient/build/CMakeFiles/ZCatClient.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "RC"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_RC
+  "C:/Users/35474/Desktop/GitRepos/ZCat/ZCatClient/resource/app_ico/ZCat.rc" "C:/Users/35474/Desktop/GitRepos/ZCat/ZCatClient/build/CMakeFiles/ZCatClient.dir/resource/app_ico/ZCat.rc.obj"
+  )
+
+# The include file search paths:
+set(CMAKE_RC_TARGET_INCLUDE_PATH
+  "C:/Users/35474/Desktop/GitRepos/ZCat/ZCatClient/source/include"
+  "C:/Users/35474/Desktop/GitRepos/ZCat/ZCatClient/include/SDL2"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/35474/Desktop/GitRepos/ZCat/ZCatClient/source/source/GraphicOutput.cpp" "CMakeFiles/ZCatClient.dir/source/source/GraphicOutput.cpp.obj" "gcc" "CMakeFiles/ZCatClient.dir/source/source/GraphicOutput.cpp.obj.d"
+  "C:/Users/35474/Desktop/GitRepos/ZCat/ZCatClient/source/source/TcpClient.cpp" "CMakeFiles/ZCatClient.dir/source/source/TcpClient.cpp.obj" "gcc" "CMakeFiles/ZCatClient.dir/source/source/TcpClient.cpp.obj.d"
+  "C:/Users/35474/Desktop/GitRepos/ZCat/ZCatClient/source/source/main.cpp" "CMakeFiles/ZCatClient.dir/source/source/main.cpp.obj" "gcc" "CMakeFiles/ZCatClient.dir/source/source/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ZCatClient/build/CMakeFiles/ZCatClient.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "C:/Users/35474/Desktop/GitRepos/ZCat/ZCatClient/bin/ZCatClient.exe"
+  "C:/Users/35474/Desktop/GitRepos/ZCat/ZCatClient/bin/ZCatClient.exe.manifest"
+  "C:/Users/35474/Desktop/GitRepos/ZCat/ZCatClient/bin/ZCatClient.pdb"
+  "CMakeFiles/ZCatClient.dir/resource/app_ico/ZCat.rc.obj"
+  "CMakeFiles/ZCatClient.dir/source/source/GraphicOutput.cpp.obj"
+  "CMakeFiles/ZCatClient.dir/source/source/GraphicOutput.cpp.obj.d"
+  "CMakeFiles/ZCatClient.dir/source/source/TcpClient.cpp.obj"
+  "CMakeFiles/ZCatClient.dir/source/source/TcpClient.cpp.obj.d"
+  "CMakeFiles/ZCatClient.dir/source/source/main.cpp.obj"
+  "CMakeFiles/ZCatClient.dir/source/source/main.cpp.obj.d"
+  "libZCatClient.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX RC)
+  include(CMakeFiles/ZCatClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
